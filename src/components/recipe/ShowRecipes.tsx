bf299b7ff244fc42be315b11ce9c029f1c210b81
Kendrick Tan <kendricktan0814@gmail.com>
2020-06-19 12:44:03 +1000

    filter

--- src/components/recipe/ShowRecipes.tsx
+++ src/components/recipe/ShowRecipes.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 
-import { Spacer, Image, Display, Loading, Row, Text } from "@zeit-ui/react";
+import {
+  Input,
+  Spacer,
+  Image,
+  Display,
+  Loading,
+  Row,
+  Text,
+} from "@zeit-ui/react";
 
 import useGetGistContent from "../../containers/GetGistContent";
 import useGetGitHubGistId from "../../containers/GetGitHubGistId";
@@ -10,6 +18,8 @@ const ShowRecipes = () => {
   const { setIsModalOpen, setSelectedRecipeId } = useRecipeModal.useContainer();
   const { gistId } = useGetGitHubGistId.useContainer();
   const { isGetting, gistContent } = useGetGistContent.useContainer();
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   const recipeIds = Object.keys(gistContent || {});
 
@@ -31,36 +41,72 @@ const ShowRecipes = () => {
 
   return (
     <>
-      {recipeIds.map((recipeId) => {
-        const { image, title } = gistContent[recipeId];
+      <Text h2>Recipes</Text>
+      <Input
+        value={searchTerm}
+        width="100%"
+        placeholder="Search"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {recipeIds
+        .filter((recipeId) => {
+          const searchTermStandardized = searchTerm.trim().toLowerCase();
+          const { title, tags } = gistContent[recipeId];
 
-        return (
-          <>
-            <Display
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                setIsModalOpen(true);
-                setSelectedRecipeId(recipeId);
-              }}
-              shadow
-              caption={
-                <span style={{ display: "block", marginTop: "-1rem" }}>
-                  {title}
-                </span>
+          // Display everything by default
+          if (searchTermStandardized === "") {
+            return true;
+          }
+
+          // Search by tag
+          if (
+            searchTermStandardized.slice(0, 5) === "tags:" ||
+            searchTermStandardized.slice(0, 4) === "tag:"
+          ) {
+            const searchTags = searchTermStandardized.split(":")[1].split(",");
+
+            for (const searchTag of searchTags) {
+              for (const tag of tags) {
+                if (tag.includes(searchTag)) {
+                  return true;
+                }
               }
-            >
-              <Image
-                width={435}
-                height={200}
-                src={image}
-                style={{ objectFit: "cover", maxHeight: "200px" }}
-              />
-            </Display>
+            }
+          }
 
-            <Spacer y={1} />
-          </>
-        );
-      })}
+          // Else search by name
+          return title.toLowerCase().includes(searchTermStandardized);
+        })
+        .map((recipeId) => {
+          const { image, title } = gistContent[recipeId];
+
+          return (
+            <>
+              <Display
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setIsModalOpen(true);
+                  setSelectedRecipeId(recipeId);
+                }}
+                shadow
+                caption={
+                  <span style={{ display: "block", marginTop: "-1rem" }}>
+                    {title}
+                  </span>
+                }
+              >
+                <Image
+                  width={435}
+                  height={200}
+                  src={image}
+                  style={{ objectFit: "cover", maxHeight: "200px" }}
+                />
+              </Display>
+
+              <Spacer y={1} />
+            </>
+          );
+        })}
     </>
   );
 };
